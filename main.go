c1ad227f7431422a12dfaab726b9afa8dc9946f5
unknown <rafael@rafaelromero.info>
2023-11-09 19:08:16 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	// Add fields to hold the state of your application.
+}
+
+type msg struct {
+	// Add fields to represent the different types of messages your application can handle.
+}
+
+func (m model) Init() tea.Cmd {
+	// Initialize your application and return any commands you want to run.
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// Handle the different types of messages and return the updated model and any commands you want to run.
+	return m, nil
+}
+
+func (m model) View() string {
+	// Return a string representation of your model.
+	return ""
+}
+
+func main() {
+	p := tea.NewProgram(model{})
+	if err := p.Start(); err != nil {
+		// Handle any errors that occurred and exit.
+		os.Exit(1)
+	}
+}
